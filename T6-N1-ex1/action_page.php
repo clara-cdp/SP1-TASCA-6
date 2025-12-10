b51ff99615cac23ef78f00b4186cf24a7dc9fdec
clara-cdp <clarianne.cdp@gmail.com>
2025-12-10 10:57:18 +0100

    applied css and php properties

--- T6-N1-ex1/action_page.php
+++ T6-N1-ex1/action_page.php
@@ -1,12 +1,49 @@
 <html>
+<link rel="stylesheet" href="styles.css">
 
 <body>
+    <div class=Questionare>
 
-    <?php $userName = htmlspecialchars($_POST['username']); //?? 'Guest');
-    $userEmail = htmlspecialchars($_POST['email']); // ?? 'No email provided');
+        <style>
+            html {
+                text-align: center;
+            }
+        </style>
+        <?php
 
-    echo "Hello, $userName. Welcome to our page!.<br>";
-    echo "Your email $userEmail"; ?>
+
+        $userName = isset($_POST['username']) ? htmlspecialchars($_POST['username']) : 'Guest';
+        $userEmail = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : 'No email provided';
+
+        echo "<p>Hello, $userName. We welcome to our community!.</p><br>";
+        echo "Our newsletter will be sent to your email account $userEmail<br>";
+
+        echo "<h3> Your chosen fate: </h3>";
+
+        $monster = isset($_POST['monster']) && is_array($_POST['monster']) ? $_POST['monster'] : [];
+
+        $selected_count = count($monster);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $vampCheked = in_array('vampire', $monster);
+            $wereCheked = in_array('werewolf', $monster);
+
+            if ($selected_count === 1) {
+                if ($vampCheked) {
+                    echo "<p>🧛<br> Be careful what you wish for, especially during the season of the dead.</p>";
+                } else {
+                    echo "<p>🐺<br>You can now howl at the moon..., but not so loud as to disturb the neighbors.";
+                }
+            } else {
+                if ($selected_count === 0) {
+                    echo "<p>You chose a path of boring, un-monstrous neutrality.</p>";
+                } else {
+                    echo "<p> You choose poorly. And will burn before the gates of hell</p>";
+                }
+            }
+        }
+
+        ?>
+    </div>
 
 </body>
 
