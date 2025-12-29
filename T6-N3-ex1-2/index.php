d8cc3139ab80e766e16bf6ad3c910d65b14c65ba
clara-cdp <clarianne.cdp@gmail.com>
2025-12-29 19:50:00 +0100

    removed everything, starting over

--- T6-N3-ex1-2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-use Cocur\Slugify\Slugify;
-
-$slugify = new slugify();
-
-echo $slugify->slugify("I'm gonna make him an offer he can't refuse.");
